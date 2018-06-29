27cb2c26eaa6cfe81ca9db5f31e8849e83f4e398
Yan Alves <yanalves@hotmail.com>
2018-06-29 03:42:39 -0300

    :recycle: Refatora método de calcula de desconto por tempo

--- GerenciadorDeDesconto/GerenciadorDeDesconto/GerenciadorDeDesconto.cs
+++ GerenciadorDeDesconto/GerenciadorDeDesconto/GerenciadorDeDesconto.cs
@@ -1,69 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace GerenciadorDeDesconto
+﻿namespace GerenciadorDeDesconto
 {
     public class GerenciadorDeDesconto
     {
+        private decimal procentagemDeDescontoMaximaBaseadoNoTempoDeConta = 0.05m;
+
         public decimal AplicarDesconto(decimal valorOriginal, Conta conta)
         {
-            decimal precoComDesconto = 0;
-
             decimal descontoBaseadoEmTempoDeConta = CalcularDescontoBaseadoEmTempoDeConta(conta);
+            decimal descontoBaseadoEmTipoDeConta = ObterDescontoBaseadoEmTipoDeConta(conta);
+
+            return CalcularPrecoTotalComDescontos(valorOriginal, descontoBaseadoEmTempoDeConta, descontoBaseadoEmTipoDeConta);
+        }
+
+        private decimal CalcularDescontoBaseadoEmTempoDeConta(Conta conta)
+        {
+            return (conta.TempoDeCadastroEmAnos > 5) ? procentagemDeDescontoMaximaBaseadoNoTempoDeConta : (decimal)conta.TempoDeCadastroEmAnos / 100;
+        }
+
+        public decimal ObterDescontoBaseadoEmTipoDeConta(Conta conta)
+        {
             switch (conta.Tipo)
             {
-                case TipoDeConta.Visitante:
-                    precoComDesconto = valorOriginal;
-                    break;
-
                 case TipoDeConta.Cliente:
-                    precoComDesconto = (valorOriginal - (0.1m * valorOriginal)) - descontoBaseadoEmTempoDeConta * (valorOriginal - (0.1m * valorOriginal));
-                    break;
+                    return 0.1m;
 
                 case TipoDeConta.Vip:
-                    precoComDesconto = (0.7m * valorOriginal) - descontoBaseadoEmTempoDeConta * (0.7m * valorOriginal);
-                    break;
+                    return 0.7m;
 
                 case TipoDeConta.Socio:
-                    precoComDesconto = (valorOriginal - (0.5m * valorOriginal)) - descontoBaseadoEmTempoDeConta * (valorOriginal - (0.5m * valorOriginal));
-                    break;
+                    return 0.5m;
+
+                default:
+                    return 0m;
             }
-
-            return precoComDesconto;
         }
-
-        public decimal CalcularDescontoBaseadoEmTempoDeConta(Conta conta)
+        private decimal CalcularPrecoTotalComDescontos(decimal valorOriginal, decimal descontoBaseadoEmTempoDeConta, decimal descontoBaseadoEmTipoDeConta)
         {
-            return (conta.TempoDeCadastroEmAnos > 5) ? (decimal)5 / 100 : (decimal)conta.TempoDeCadastroEmAnos / 100;
+            return (valorOriginal - (descontoBaseadoEmTipoDeConta * valorOriginal)) - descontoBaseadoEmTempoDeConta * (valorOriginal - (descontoBaseadoEmTipoDeConta * valorOriginal));
         }
-
-        //public decimal CalcularPrecoComDesconto(decimal amount, int type, int years)
-        //{
-        //    decimal result = 0;
-
-        //    decimal disc = (years > 5) ? (decimal)5 / 100 : (decimal)years / 100;
-
-        //    if (type == 1)
-        //    {
-        //        result = amount;
-        //    }
-        //    else if (type == 2)
-        //    {
-        //        result = (amount - (0.1m * amount)) - disc * (amount - (0.1m * amount));
-        //    }
-        //    else if (type == 3)
-        //    {
-        //        result = (0.7m * amount) - disc * (0.7m * amount);
-        //    }
-        //    else if (type == 4)
-        //    {
-        //        result = (amount - (0.5m * amount)) - disc * (amount - (0.5m * amount));
-        //    }
-
-        //    return result;
-        //}
     }
 }
